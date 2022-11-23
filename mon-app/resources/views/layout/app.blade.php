50b101a84e207b639b77fac1920b54e92fcb1951
Marlee Alcaraz <marlee.alcaraz@gmail.com>
2022-11-23 14:04:43 +0100

    commit 2

---
+++ mon-app/resources/views/layout/app.blade.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title>Séries - @yield('title')</title>
+    </head>
+    <body>
+        <header>
+            @section('navbar')
+                
+            @show
+        </header>
+ 
+        <footer>
+            @yield('content')
+        </footer>
+    </body>
+</html>
---
+++ mon-app/resources/views/layout/child.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('title', 'Séries')
+
+@section('navbar')
+<h2>Menu</h2>
+@endsection
+
+@section('content')
+<p>&copy;Marlee</p>
+@endsection
